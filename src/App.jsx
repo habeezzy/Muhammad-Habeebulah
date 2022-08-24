cb62a6e36ebb28e6b1d3d05060ec6aae6a082c36
Muhammad <mhabeebulah@gmail.com>
2022-08-23 20:07:19 -0400

    CSS variables complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Header from './components/header/Header'
 import Nav from './components/nav/Nav'
+import About from './components/about/About'
 import Experience from './components/experience/Experience'
 import Services from './components/services/Services'
 import Portfolio from './components/portfolio/Portfolio'
@@ -15,6 +16,7 @@ const App = () => {
     <>
     <Header/>
     <Nav/>
+    <About/>
     <Experience/>
     <Services/>
     <Portfolio/>
