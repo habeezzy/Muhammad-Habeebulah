ff7aace77b1c5da9f9e3750334e22c8474900012
Muhammad <mhabeebulah@gmail.com>
2022-09-10 20:32:17 -0400

    Header Component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
 
 const header = () => {
   return (
-    <div>header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Muhammad Habeebulah</h1>
+        <h1 className="text-light">Fullstack Developer</h1>
+        <CTA/>
+      </div>
+
+    </header>
   )
 }
 
 export default header
+
+
+
+
+
