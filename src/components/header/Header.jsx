45d59b945234b121ee0cf074f967bbdbdf74789c
Muhammad <mhabeebulah@gmail.com>
2022-09-28 14:43:11 -0400

    header component complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,13 +10,14 @@ const header = () => {
       <div className="container header__container">
         <h5>Hello I'm</h5>
         <h1>Muhammad Habeebulah</h1>
-        <h1 className="text-light">Fullstack Developer</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
         <CTA/>
         <HeaderSocial />
         <div className="me" >
           <img src= {ME} alt=""/>
         </div>
-        src
+        
+        <a href="#contact" className="scroll__down">Scroll Down</a>
       </div>
 
     </header>
--- src/components/header/headerSocials.jsx
+++ src/components/header/headerSocials.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import {BsLinkedin} from 'react-icons/bs'
-import {BsGithub} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
 import {GrDribbble} from 'react-icons/gr'
 
 const headerSocials = () => {
   return (
-    <div>
+    <div className="header__socials">
         <a href="https://linkedin.com/in/muhammad-habeebulah" target="_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target="_blank"><BsGithub/></a>
-        <a href="https://dribble.com" target="_blank"><GrDribbble/></a>
+        <a href="https://github.com/habeezzy" target="_blank"><FaGithub/></a>
+        <a href="https://dribble.com/habeezzy" target="_blank"><GrDribbble/></a>
     </div>
   )
 }
